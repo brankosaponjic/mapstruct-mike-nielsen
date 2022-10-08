11440650f0ac355c5c549a05ab4d04ba53311983
Branko Saponjic <branko.saponjic@programerika.com>
2022-10-08 18:39:19 +0200

    init commit

---
+++ src/main/java/com/brankosaponjic/mapstructmikenielsen/MapstructMikeNielsenApplication.java
@@ -0,0 +1,13 @@
+package com.brankosaponjic.mapstructmikenielsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructMikeNielsenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapstructMikeNielsenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brankosaponjic/mapstructmikenielsen/MapstructMikeNielsenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brankosaponjic.mapstructmikenielsen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructMikeNielsenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
